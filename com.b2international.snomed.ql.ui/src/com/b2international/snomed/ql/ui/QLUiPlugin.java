1b702b220f1acf6ca661c1997dad0d8e991aec9d
Mark Czotter <mczotter@b2international.com>
2021-04-19 17:31:55 +0200

    feat(releng): inject additional ECL modules when creating QL injector
    SO-4728

---
+++ com.b2international.snomed.ql.ui/src/com/b2international/snomed/ql/ui/QLUiPlugin.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snomed.ql.ui;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.util.Modules2;
+
+import com.b2international.snomed.ql.ui.internal.QlActivator;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+/**
+ * @since 0.1.0
+ */
+public class QLUiPlugin extends QlActivator {
+
+	public static QLUiPlugin getDefault() {
+		return (QLUiPlugin) getInstance();
+	}
+	
+	@Override
+	protected Injector createInjector(String language) {
+		try {
+			com.google.inject.Module runtimeModule = getRuntimeModule(language);
+			// additional core language bindings go here via extension point
+			for (Module additionalRuntimeModule : getExtensions("com.b2international.snomed.ecl.ui.eclAdditionalModules", "class", Module.class)) {
+				runtimeModule = Modules2.mixin(runtimeModule, additionalRuntimeModule);
+			}
+			
+			com.google.inject.Module sharedStateModule = getSharedStateModule();
+			com.google.inject.Module uiModule = getUiModule(language);
+			com.google.inject.Module mergedModule = Modules2.mixin(runtimeModule, sharedStateModule, uiModule);
+			return Guice.createInjector(mergedModule);
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to create injector for " + language, e);
+		}
+	}
+	
+	private static final <T> Collection<T> getExtensions(final String extensionPoint, final String classAttributeName, final Class<T> type) {
+		checkNotNull(extensionPoint, "extensionPoint");
+		checkNotNull(classAttributeName, "classAttributeName");
+		checkNotNull(type, "type");
+		final Collection<T> extensions = newArrayList();
+		final IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(extensionPoint);
+		for (final IConfigurationElement element : elements) {
+			try {
+				if (hasAttributeOf(element, classAttributeName)) {
+					final Object object = checkNotNull(element, "element").createExecutableExtension(classAttributeName);
+					extensions.add(type.cast(object));
+				}
+			} catch (final CoreException e) {
+				throw new RuntimeException(String.format("Exception happened when creating element from %s bundle's extension: %s", element
+						.getContributor().getName(), extensionPoint), e);
+			}
+		}
+		return extensions;
+	}
+	
+	/*Returns with true if the configuration element has the given attribute. Otherwise returns with false.*/
+	private static boolean hasAttributeOf(final IConfigurationElement element, final String attributeName) {
+		return newHashSet(element.getAttributeNames()).contains(attributeName);
+	}
+	
+	public final Image getImage(String relativePath) {
+		Image image = getImageRegistry().get(relativePath);
+		if (image == null) {
+			final ImageDescriptor imageDescriptor = getImageDescriptor(relativePath);
+			if (imageDescriptor != null) {
+				image = imageDescriptor.createImage();
+			}
+		}
+		return image;
+	}
+	
+	public final ImageDescriptor getImageDescriptor(String relativePath) {
+		
+		if (relativePath == null) return null;
+		
+		ImageDescriptor descriptor = getImageRegistry().getDescriptor(relativePath);
+		if (descriptor == null) {
+			descriptor = AbstractUIPlugin.imageDescriptorFromPlugin(getBundle().getSymbolicName(), relativePath);
+			getImageRegistry().put(relativePath, descriptor);
+		}
+		return descriptor;
+	}
+		
+}
